183fcfda8d5fd00e9b634f7dbc6a09b6def0eadf
Harenderchhoker31 <harenderchhoker31@gmail.com>
2025-12-08 12:14:27 +0530

    Done

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001';
+// const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://erp-managment-system-xx77.vercel.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
